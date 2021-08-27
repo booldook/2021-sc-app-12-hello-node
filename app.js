396c47c4b42bd8a9b3595a3e8dcf06d9b1d30cc4
booldook <booldook@gmail.com>
2021-08-27 17:01:07 +0900

    first init

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const port = 4000
+/*************** global init **************/
+const port = 3000
 const express = require('express')
 const app = express()
-const { random, serverInit } = require('./modules/util')
 
-app.get()
 
-/************* server start *************/
-app.listen(port, serverInit(port))
+/*************** router init **************/
+app.get('/', (req, res, next) => {})
+
+
+/*************** server init **************/
+app.listen(port, () => { console.log('http://127.0.0.1:'+port) })
