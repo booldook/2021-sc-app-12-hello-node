d29127f21e5deafdd9eb66991cf2644fb7831e8d
booldook <booldook@gmail.com>
2021-08-30 11:23:10 +0900

    FrontEnd 구조 적용

---
+++ app.js
@@ -0,0 +1,14 @@
+// 정적 HTML, 동적 HTML
+
+/*************** global init **************/
+const port = 3000
+const express = require('express')
+const app = express()
+
+
+/*************** router init **************/
+app.get('/', (req, res, next) => {})
+
+
+/*************** server init **************/
+app.listen(port, () => { console.log('http://127.0.0.1:'+port) })
